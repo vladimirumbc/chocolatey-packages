66f6edb26300ece398883f265a26d69c92810f8a
Vladimir Plotkin <vladimir@umbc.edu>
2022-09-13 10:19:16 -0400

    Added install location option to the installer

--- automatic/putty.install/tools/chocolateyInstall.ps1
+++ automatic/putty.install/tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$pp = Get-PackageParameters
 
 $packageArgs = @{
     PackageName    = "putty.install"
@@ -10,6 +11,10 @@ $packageArgs = @{
     File64         = "$toolsPath\putty-64bit-0.77-installer.msi"
     SilentArgs     = '/qn /norestart /l*v "{0}\install.log"' -f "$Env:TEMP\chocolatey\$Env:ChocolateyPackageName\$Env:ChocolateyPackageVersion"
     ValidExitCodes = @(0)
+}
+
+if ($pp.installLocation) {
+	$packageArgs["SilentArgs"] += ' INSTALLDIR="' + $pp.installLocation + '"'
 }
 Install-ChocolateyInstallPackage @packageArgs
 Remove-Item -Force "$toolsPath\*.msi","$toolsPath\*.ignore" -ea 0
